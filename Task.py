c0d9bf2138c1933622c41f28a24b01db23799d1d
Luis <tata27_2002@hotmail.com>
2025-05-10 23:44:24 -0600

    Task class implemented. Create and update methods

--- Task.py
+++ Task.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 
-
 class Task:
     #Status Constants
     TODO = "todo"
@@ -13,7 +12,7 @@ class Task:
 
     def __init__(self, description):
         #Increment it before assigning to a task
-        Task.counter =+ 1
+        Task.counter += 1
 
         self.id = Task.counter
         self.status = Task.TODO
@@ -22,6 +21,9 @@ class Task:
         #Store the current date when a task is created
         now = datetime.now()
         self.createdAt = self.updatedAt = now.strftime("%Y-%m-%d %H:%M")
+    
+    def __str__(self):
+        return f"{self.id}\t{self.description}\t{self.status}\t{self.updatedAt}\t{self.createdAt}"
     
     def updateDescription(self, newDescription):
 
@@ -36,9 +38,10 @@ class Task:
         validStatus = [self.DONE, self.TODO, self.IN_PROGRESS]
 
         if newStatus not in validStatus:
-            return print("Wrong status....")
+            return print("Invalid status....")
         
         self.status = newStatus
+        #Setting time when it was updated
+        now = datetime.now()
+        self.updatedAt = now.strftime("%Y-%m-%d %H:%M")
     
-    
-
